b0c2e2aa880f647c950f69b372dd7f14a6341106
EmiTorres7 <emi.mtorresbq@gmail.com>
2026-02-28 13:07:20 -0300

    refactor: update views router to use ProductManager and implement product retrieval

--- entregable_2/src/routers/views.router.js
+++ entregable_2/src/routers/views.router.js
@@ -1,20 +1,23 @@
 import express from 'express';
-//import { Router } from 'express';
-//const router = Router();
-
-//import ProductManager from './managers/ProductManager.js';
+import ProductManager from '../utils/ProductoManager.js';
 //import CartManager from './managers/CartManager.js';
 
 //creamos la variable que va a manejar todas las rutas relacionadas a las vistas
 const viewsRouter = express.Router();
 
 //creamos una instancia de ProductManager y CartManager para poder usar sus metodos en las rutas
-//const productManager = new ProductManager('./database/products.json');
+const productManager = new ProductManager('./src/data/products.json');
 //const cartManager = new CartManager('./database/carts.json');
 
 //creamos las rutas para las vistas
-viewsRouter.get('/', (req, res) => {
-    res.render('home');
+viewsRouter.get('/', async (req, res) => {
+    try {
+        const products = await productManager.getProducts();
+        res.render('home', { products });
+    } catch (error) {
+        console.error("Error al obtener los productos para la vista home.", error);
+        res.status(500).send({ message: error.message });
+    }
 });
 
 //hacemos un export default para importar este views router en el app.js y usarlo como middleware para las rutas relacionadas a las vistas.
