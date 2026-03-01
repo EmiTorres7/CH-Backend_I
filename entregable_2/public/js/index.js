b594efc6d79bd78cbba773b52f46bf97c4415880
EmiTorres7 <emi.mtorresbq@gmail.com>
2026-03-01 13:23:07 -0300

    feat: add product submission form and implement real-time product updates via WebSocket

--- entregable_2/public/js/index.js
+++ entregable_2/public/js/index.js
@@ -2,3 +2,27 @@
 const socket = io();
 
 //escuchamos el evento 'productAdded' que se emite desde el servidor cuando se agrega un nuevo producto
+
+const formNewProduct = document.getElementById('formNewProduct');
+
+formNewProduct.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(formNewProduct);
+    const productData = {}
+    formData.forEach((value, key) => {
+        productData[key] = value;
+    });
+    console.log(productData);
+    //enviamos los datos del productos al servidor para guardarlo en el json
+    socket.emit('newProduct', productData);
+});
+
+socket.on('productAdded', (newProduct) => {
+    console.log('Nuevo producto agregado:', newProduct);
+    // Aquí puedes actualizar la interfaz de usuario para mostrar el nuevo producto
+    // Por ejemplo, podrías agregar el nuevo producto a una lista en la página
+    const productsList = document.getElementById('productsList');
+    productsList.innerHTML += `<li>${newProduct.title} - $${newProduct.price}</li>`;
+});
+
+
--- entregable_2/src/app.js
+++ entregable_2/src/app.js
@@ -7,10 +7,11 @@ import viewsRouter from './routers/views.router.js';
 import { engine } from 'express-handlebars';
 import { Server } from 'socket.io';
 import http from 'http';
+import ProductManager from './utils/ProductoManager.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-//import { product } from './managers/productManager.js';
+
 //import { cart } from './managers/cartManager.js';
 
 const app = express();
@@ -39,9 +40,20 @@ app.use('/', viewsRouter);
 
 
 //WEBSOCKETS
+const productManager = new ProductManager('./src/data/products.json');
 io.on('connection', (socket) => {
     console.log('Nuevo cliente conectado');
+    socket.on('newProduct', async (productData) => {
+        try {
+            const newProduct = await productManager.addProduct(productData);
+            console.log('Nuevo producto agregado:', newProduct);
+            io.emit('productAdded', newProduct.data); // Emitir a todos los clientes conectados
+        } catch (error) {
+            console.error('Error al agregar producto:', error);
+        }
+    });
 });
+
 
 
 //iniciamos el servidor y escuchamos en el puerto definido
