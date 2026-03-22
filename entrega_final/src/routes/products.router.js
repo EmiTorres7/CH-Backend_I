c54b82cb8974a428e44cc6cba3a2d967a9d92b07
EmiTorres7 <emi.mtorresbq@gmail.com>
2026-03-22 11:03:27 -0300

    feat: enhance product retrieval and add update functionality with error handling

--- entrega_final/src/routes/products.router.js
+++ entrega_final/src/routes/products.router.js
@@ -5,7 +5,7 @@ const productsRouter = express.Router()
 
 productsRouter.get("/", async (req, res) => {
     try {
-        const products = await Product.find();
+        const products = await Product.find().lean();
         res.status(200)
             .json({ status: "success", payload: products })
     } catch (error) {
@@ -14,19 +14,37 @@ productsRouter.get("/", async (req, res) => {
 })
 
 //endpoint para crear/añadir un producto nuevo
-productsRouter.post("/", async(req,res) =>{
+productsRouter.post("/", async (req, res) => {
     try {
         //toda la data que envío a través de post llega en el objeto de req, dentro de body está toda la info del producto.
         const productData = req.body;
         const newProduct = await Product.create(productData);  //de forma interna va a crear el producto en la colección y me devuelve la inifo de ese producto ya creado (la data que ya había enviado más los datos que cree de forma automática como el id o los campos de creación y edición, todo esto lo capturamos en esat variable). Aquí tenemos el nuevo producto luego de que fue añadido a la bdd
-        
+
         //una vez añadido, enviar la respuesta de mi servidor
-        res.status(201).json({status: "success", payload: newProduct}) //acá es product xq voy a agregar la info de ese producto nuevo, en la otra era products xq me traía varios productos.
+        res.status(201).json({ status: "success", payload: newProduct }) //acá es product xq voy a agregar la info de ese producto nuevo, en la otra era products xq me traía varios productos.
 
     } catch (error) {
-        res.status(500).json({ status: "error", message: "Error al agregar un producto nuevo"})
+        res.status(500).json({ status: "error", message: "Error al agregar un producto nuevo", error })
     }
-
 })
+
+//endpoint para editar la información de un producto
+productsRouter.put("/:pid", async (req, res) => {
+    try {
+        const pid = req.params.pid; //capturamos el id del producto que quiero editar
+        //capturamos las actualizaciones, necesitamos el objeto con las actualizaciones que nos mandan a través del body
+        const updateData = req.body;
+
+        //guardar los datos del producto que fue modificado
+        const updatedProduct = await Product.findByIdAndUpdate(pid, updateData, { new: true, runValidators: true });
+        if (!updatedProduct) return res.status(404).json({ status: "error", message: "Producto no encontrado" });
+        res.status(200).json({ status: "success", payload: updatedProduct });
+
+    } catch (error) {
+        res.status(500).json({ status: "error", message: "Error al editar un producto", error })
+    }
+})
+
+
 
 export default productsRouter;
