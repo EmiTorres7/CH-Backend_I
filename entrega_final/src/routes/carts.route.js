fb93d00e9d76d0e0edeb7f93bd1ce3df8b76759b
EmiTorres7 <emi.mtorresbq@gmail.com>
2026-03-23 13:08:04 -0300

    fix: correct retrieval of cart ID in get cart endpoint

--- entrega_final/src/routes/carts.route.js
+++ entrega_final/src/routes/carts.route.js
@@ -32,7 +32,7 @@ cartRouter.post("/:cid/product/:pid", async (req, res) => {
 //endpoint para traernos los productos del carrito
 cartRouter.get("/:cid", async (req, res) => {
     try {
-        const cid = req.params;
+        const cid = req.params.cid;
         const cart = await Cart.findById(cid); //si no encuentra el id no nos va a devovler nada en la variable cart
         if (!cart) return res.status(404).json({ status: "error", message: "Carrito no encontrado" })
         res.status(200).json({ status: "success", payload: cart.products });
