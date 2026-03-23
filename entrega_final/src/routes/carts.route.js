71ea7a417ea4ecb98c76709e1dce47300771daa0
EmiTorres7 <emi.mtorresbq@gmail.com>
2026-03-23 12:29:49 -0300

    fix: correct method to update cart by ID and ensure proper retrieval of cart products

--- entrega_final/src/routes/carts.route.js
+++ entrega_final/src/routes/carts.route.js
@@ -22,10 +22,22 @@ cartRouter.post("/:cid/product/:pid", async (req, res) => {
         const { quantity } = req.body;
 
         //guardar actualizaciones
-        const updatedCart = await Cart.findOneAndUpdate(cid, { $push: { products: { product: pid, quantity: quantity } } }, { new: true, runValidators: true });
+        const updatedCart = await Cart.findByIdAndUpdate(cid, { $push: { products: { product: pid, quantity: quantity } } }, { new: true, runValidators: true });
         res.status(200).json({ status: "success", payload: updatedCart });
     } catch (error) {
         res.status(500).json({ status: "error", message: "Error al agregar un producto al carrito" })
+    }
+})
+
+//endpoint para traernos los productos del carrito
+cartRouter.get("/:cid", async (req, res) => {
+    try {
+        const cid = req.params;
+        const cart = await Cart.findById(cid); //si no encuentra el id no nos va a devovler nada en la variable cart
+        if (!cart) return res.status(404).json({ status: "error", message: "Carrito no encontrado" })
+        res.status(200).json({ status: "success", payload: cart.products });
+    } catch (error) {
+        res.status(500).json({ status: "error", message: "Error al recuperar los productos del carrito" })
     }
 })
 
