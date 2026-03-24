13342c8b333ae5047f91c708785c28af49066b85
EmiTorres7 <emi.mtorresbq@gmail.com>
2026-03-24 13:10:27 -0300

    feat: implement product pagination and render products in home view

--- entrega_final/src/routes/views.router.js
+++ entrega_final/src/routes/views.router.js
@@ -1,10 +1,24 @@
 import express from "express";
+import Product from  "../models/products.model.js";
 
 //manejador de las rutas para tener todas las funcionalidades para poder crear las rutas
 const viewsRouter = express.Router();
 
-viewsRouter.get("/", async(req,res) =>{
-    res.render("home");
+viewsRouter.get("/", async (req, res) => {
+    try {
+        //tapturar los queryparams
+        const { limit=10, page=1 } = req.query;
+
+        //guardar la data que nos llegue de la consulta del await con el modelo de porducto
+        const data = await Product.paginate({ }, {limit, page, lean:true});
+        const products = data.docs;
+        delete data.docs
+
+        //renderizar mostrar la página
+        res.render("home", { products });
+    } catch (error) {
+        res.status(500).json({ status: "error", message: "Error al mostrar la página" })
+    }
 });
 
 export default viewsRouter;
